de3e03c3a860c8546a1197112f19b521d33e1aab
kirill217 <kirillkaya@mail.ru>
2022-04-13 16:16:02 +0300

    Первое задание 8 урока

---
+++ 8.1.py
@@ -0,0 +1,24 @@
+import re
+# pattern = r'^[a-z,A-Z,0-9,\.,\_]+@[a-z]+\.[a-z]+'
+
+# RE_NAME = re.compile(pattern)
+# email_name = input('Введите email:' )
+# print(RE_NAME.match('Kirillkaya@mail.ru'))
+# print(RE_NAME.match('Kirillkayamail.ru'))
+# print(RE_NAME.match('Kirillkaya@mailru'))
+# print(RE_NAME.match('Kirill217kaya@mail.ru'))
+# print(RE_NAME.match(email_name))
+
+# name_email = input('Введите email: ')
+
+def correct_email(email):
+    pattern = r'^[a-z,A-Z,0-9,\.,\_]+@[a-z]+\.[a-z]+$'
+    RE_NAME = re.compile(pattern)
+    if RE_NAME.match(email) is None:
+        print('ValueError: wrong email:', email)
+    else:
+        print('Ваш email:', email)
+
+    return
+
+correct_email('krill217sulimov@gmail.com')
